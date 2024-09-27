a620964735d25c545480f982b8959eabe5ffbc02
edvinbakalovic <edvin.bakalovic04@gmail.com>
2024-09-27 22:59:45 +0200

    Ispravka nekih dijelova

--- react/src/App.js
+++ react/src/App.js
@@ -1,9 +1,11 @@
 
+import { useState } from 'react';
 import './App.css';
 import Task from './Task.js';
 import TaskForm from './TaskForm.js';
 
 function App() {
+  const [tasks,setTasks] = useState([]);
   return (
    <main>
       <TaskForm/>
--- react/src/Task.js
+++ react/src/Task.js
@@ -4,7 +4,8 @@ import Checkbox from "./Checkbox";
 export default function Task(){
     return(
         <div className="task">
-            <Checkbox /> 
+            <Checkbox defaultChecked={true} />  
+            test task
         </div>
     );
 }
