e043c79726b137c76c5b42c55280fa9b16b349f5
edvinbakalovic <edvin.bakalovic04@gmail.com>
2024-09-28 23:13:16 +0200

    Dodan local storage!

--- react/src/App.js
+++ react/src/App.js
@@ -1,11 +1,21 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Task from './Task.js';
 import TaskForm from './TaskForm.js';
 
 function App() {
   const [tasks,setTasks] = useState([]);
+  useEffect(()=>{
+    if(tasks.length===0) return;
+    localStorage.setItem('tasks',JSON.stringify(tasks));
+  },[tasks])
+
+  useEffect(()=>{
+    const task = JSON.parse(localStorage.getItem('tasks'));
+    setTasks(task);
+  },[])
+
   function addTask(name){
     setTasks((prev)=>{
       return [...prev,{name:name,done:false}]
