f2229f0b9485041501f27d201c0e8d7cae8ba80d
edvinbakalovic <edvin.bakalovic04@gmail.com>
2024-09-29 19:09:06 +0200

    Dodavanje trash ikonice

--- react/src/Task.js
+++ react/src/Task.js
@@ -5,7 +5,16 @@ export default function Task({name,done,onToggle}){
     return(
         <div className={'task ' + (done?'done':'')}>
             <Checkbox checked={done} onClick={()=>onToggle(!done)} />  
-            <span>{name}</span>
+
+            <div className="task-name">
+                <span>{name}</span>
+             </div>
+
+             
+                <button className="trash">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0L284.2 0c12.1 0 23.2 6.8 28.6 17.7L320 32l96 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 96C14.3 96 0 81.7 0 64S14.3 32 32 32l96 0 7.2-14.3zM32 128l384 0 0 320c0 35.3-28.7 64-64 64L96 512c-35.3 0-64-28.7-64-64l0-320zm96 64c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16z"/></svg>
+                </button>
+             
         </div>
     );
 }
